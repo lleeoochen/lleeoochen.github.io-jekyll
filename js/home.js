fce0e6edf1ce0073934c3a7248a13a382a7c3b59
Wei Tung Chen <weitungchen1998@gmail.com>
2022-12-21 01:20:22 -0800

    improved menu icon colors

--- js/home.js
+++ js/home.js
@@ -1,22 +1,45 @@
-anime({
-  targets: '.signature path',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 1500,
-  delay: function (el, i) { return i * 150 },
-  loop: false
-});
+const isPortraitMode = window.innerHeight > window.innerWidth;
 
-anime.timeline({
-  easing: 'easeInOutSine',
-  duration: 5000,
-  loop: false,
-})
-.add({ targets: '.signature path', fill: '#ffffff' }, 0)
+anime (
+  {
+    targets: ".signature path",
+    strokeDashoffset: [
+      anime.setDashoffset,
+      0
+    ],
+    easing: "easeInOutSine",
+    duration: 1000,
+    delay: function (el, i)
+    {
+      return i * 150;
+    },
+    loop: false
+  }
+);
 
-anime({
-  targets: '#profile-pic',
-  translateX: 100,
-  opacity: 1,
-  duration: 3000
-});
+anime.timeline (
+  {
+    easing: "easeInOutSine",
+    duration: 3000,
+    loop: false
+  }
+).add (
+  {
+    targets: ".signature path",
+    fill: "#ffffff"
+  },
+  0
+);
+
+anime (
+  {
+    targets: "#profile-pic",
+    translateX: isPortraitMode ? 0 : 100,
+    translateY: isPortraitMode ? 100 : 0,
+    opacity: 1,
+    duration: 3000,
+    delay: 1500,
+    maxWidth: isPortraitMode ? "100%" : "30%",
+    maxHeight: isPortraitMode ? "30%" : "100%",
+  }
+);;
