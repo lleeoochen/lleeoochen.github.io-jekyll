c6cb471ade5e7a760ba027be29718664cc8f27f8
Wei Tung Chen <weitungchen1998@gmail.com>
2023-10-08 17:43:59 -0700

    reformmated tabs and added common font sizes

--- js/home.js
+++ js/home.js
@@ -1,45 +1,45 @@
 const isPortraitMode = window.innerHeight > window.innerWidth;
 
 anime(
-  {
-    targets: ".signature path",
-    strokeDashoffset: [
-      anime.setDashoffset,
-      0
-    ],
-    easing: "easeInOutSine",
-    duration: 1000,
-    delay: function (el, i) {
-      return i * 150;
-    },
-    loop: false
-  }
+	{
+		targets: ".signature path",
+		strokeDashoffset: [
+			anime.setDashoffset,
+			0
+		],
+		easing: "easeInOutSine",
+		duration: 1000,
+		delay: function (el, i) {
+			return i * 150;
+		},
+		loop: false
+	}
 );
 
 anime.timeline(
-  {
-    easing: "easeInOutSine",
-    duration: 3000,
-    loop: false
-  }
+	{
+		easing: "easeInOutSine",
+		duration: 3000,
+		loop: false
+	}
 ).add(
-  {
-    targets: ".signature path",
-    fill: "#d397a7",
-    stroke: "#d397a7"
-  },
-  0
+	{
+		targets: ".signature path",
+		fill: "#d397a7",
+		stroke: "#d397a7"
+	},
+	0
 );
 
 anime(
-  {
-    targets: "#profile-pic",
-    translateX: isPortraitMode ? 0 : 100,
-    translateY: isPortraitMode ? 100 : 0,
-    opacity: 1,
-    duration: 3000,
-    delay: 1500,
-    maxWidth: isPortraitMode ? "20vh" : "20vw",
-    maxHeight: isPortraitMode ? "20vh" : "20vw",
-  }
+	{
+		targets: "#profile-pic",
+		translateX: isPortraitMode ? 0 : 100,
+		translateY: isPortraitMode ? 100 : 0,
+		opacity: 1,
+		duration: 3000,
+		delay: 1500,
+		maxWidth: isPortraitMode ? "20vh" : "20vw",
+		maxHeight: isPortraitMode ? "20vh" : "20vw",
+	}
 );;
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 anime({
-  targets: '.menu',
-  easing: 'easeInOutExpo',
-  left: 0,
-  opacity: 1,
-  duration: 1000
+	targets: '.menu',
+	easing: 'easeInOutExpo',
+	left: 0,
+	opacity: 1,
+	duration: 1000
 });
