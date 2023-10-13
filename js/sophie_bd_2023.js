b5f8dbb5fb6083c537cf405f7d07979ff74f5ca5
Wei Tung Chen <weitungchen1998@gmail.com>
2023-10-13 14:19:32 -0700

    added sophie bd 2023 page

--- js/sophie_bd_2023.js
+++ js/sophie_bd_2023.js
@@ -1,13 +1,20 @@
+function onBangerClick(bangerId) {
+  $("#" + bangerId).css("opacity", "0");
+  setTimeout(() => {
+    $("#" + bangerId).css("display", "none");
+  }, 2000);
+}
+
 function onPlayClick(soundId) {
   $('.sound-platform').removeClass("show");
   $('.sound-platform.' + soundId).addClass("show");
 
   $('.sound-row').css("background-color", "unset");
-  $('.sound-row.' + soundId).css("background-color", "#00e939");
-  
-  
+  $('.sound-row.' + soundId).css("background-color", "#f9cbf4");
+
+
   $('.sound-row img').attr("src", "/assets/icons/play-button.svg");
   $('.sound-row.' + soundId + " img").attr("src", "/assets/icons/pause-button.svg");
 }
 
-onPlayClick("elgar-cello")
+// onPlayClick("boys-a-liar")
